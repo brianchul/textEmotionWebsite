10291fc163004d4f1cb5c47a9a026b03df2ade0f
brianchul <404411711@gms.tku.edu.tw>
2019-05-21 07:23:53 +0800

    update config example

--- backend/app/config/conf.example.py
+++ backend/app/config/conf.example.py
@@ -5,3 +5,12 @@ class Config(object):
 
     jwt_secret = "ITS_A_SECRET"
     jwt_expire_time = 30*60 # 5 minute
+
+    sentiment_key = "KEY"
+    sentiment_url = 'https://japaneast.api.cognitive.microsoft.com'
+    sentiment_path = '/text/analytics/v2.0/Sentiment'
+
+    translate_key = "KEY"
+    translate_base_url = 'https://api.cognitive.microsofttranslator.com'
+    translate_path = '/translate?api-version=3.0'
+    translate_params = '&to=en'
